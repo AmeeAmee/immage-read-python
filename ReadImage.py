165a875296e17cdba28eca38b76b3e73eac7f7b8
Amee <72313578+AmeeAmee@users.noreply.github.com>
2020-10-04 11:06:11 +0530

    Update ReadImage.py

--- ReadImage.py
+++ ReadImage.py
@@ -1,5 +1,5 @@
 import cv2
-img = cv2.imread('ml.png', 1)
-cv2.imshow('Output', img)
+img = cv2.imread('ml.png', 1) #'ml.png change name of your image with extension' 
+cv2.imshow('Output', img)#'output that show lable you can chage your prefrences name'
 cv2.waitKey()
 
